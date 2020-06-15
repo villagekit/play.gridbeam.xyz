e24f8a154c672ae96db03a227f51681942ed2eb9
Michael Williams <michael.williams@enspiral.com>
2020-06-15 16:33:33 +1200

    move selection hook out

--- src/selection/dom.tsx
+++ src/selection/dom.tsx
@@ -1,98 +1,11 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import {
-  doEndSelection,
-  doSetSelectionEndPoint,
-  doSetSelectionStartPoint,
-  doStartSelection,
-  getIsSelecting,
-  getIsSelectionEnabled,
-  getSelectionEndPoint,
-  getSelectionStartPoint,
-  Point,
-} from 'src'
+import { Point, useDomSelection } from 'src'
 import { Box } from 'theme-ui'
-import { MOUSE } from 'three'
 
 interface SelectionBoxProps {}
 
 export function DomSelectionBox(props: SelectionBoxProps) {
-  const dispatch = useDispatch()
-
-  const isEnabled = useSelector(getIsSelectionEnabled)
-  const isSelecting = useSelector(getIsSelecting)
-  const startPoint = useSelector(getSelectionStartPoint)
-  const endPoint = useSelector(getSelectionEndPoint)
-
-  const handleStartSelection = React.useCallback(() => {
-    dispatch(doStartSelection())
-  }, [dispatch])
-
-  const handleEndSelection = React.useCallback(() => {
-    dispatch(doEndSelection())
-  }, [dispatch])
-
-  React.useEffect(() => {
-    if (!isEnabled) handleEndSelection()
-  }, [handleEndSelection, isEnabled])
-
-  React.useEffect(() => {
-    document.addEventListener('mousedown', handleMouseDown)
-    document.addEventListener('mousemove', handleMouseMove)
-    document.addEventListener('mouseup', handleMouseUp)
-
-    return () => {
-      document.removeEventListener('mousedown', handleMouseDown)
-      document.removeEventListener('mousemove', handleMouseMove)
-      document.removeEventListener('mouseup', handleMouseUp)
-    }
-
-    function handleMouseDown(ev: MouseEvent) {
-      console.log('selection down')
-      if (!isEnabled) return
-      if (ev.altKey) return
-      if (ev.button !== MOUSE.LEFT) return
-      handleStartSelection()
-      handleStart(ev)
-    }
-
-    function handleMouseMove(ev: MouseEvent) {
-      if (!isEnabled) return
-      if (!isSelecting) return
-      handleEnd(ev)
-    }
-
-    function handleMouseUp(ev: MouseEvent) {
-      console.log('selection up')
-      if (!isEnabled) return
-      if (!isSelecting) return
-      handleEndSelection()
-      handleEnd(ev)
-    }
-
-    function handleStart(ev: MouseEvent) {
-      dispatch(
-        doSetSelectionStartPoint({
-          x: (ev.clientX / window.innerWidth) * 2 - 1,
-          y: -(ev.clientY / window.innerHeight) * 2 + 1,
-        }),
-      )
-    }
-    function handleEnd(ev: MouseEvent) {
-      dispatch(
-        doSetSelectionEndPoint({
-          x: (ev.clientX / window.innerWidth) * 2 - 1,
-          y: -(ev.clientY / window.innerHeight) * 2 + 1,
-        }),
-      )
-    }
-  }, [
-    dispatch,
-    isEnabled,
-    isSelecting,
-    handleStartSelection,
-    handleEndSelection,
-  ])
+  const { isSelecting, startPoint, endPoint } = useDomSelection()
 
   if (!isSelecting) return null
 
--- src/selection/hook.ts
+++ src/selection/hook.ts
@@ -2,7 +2,11 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useThree } from 'react-three-fiber'
 import {
+  doEndSelection,
   doSelectParts,
+  doSetSelectionEndPoint,
+  doSetSelectionStartPoint,
+  doStartSelection,
   doUpdateSelectableScreenBounds,
   getIsSelecting,
   getIsSelectionEnabled,
@@ -11,7 +15,7 @@ import {
   getSelectionStartPoint,
   Uuid,
 } from 'src'
-import { Box2, Vector2 } from 'three'
+import { Box2, MOUSE, Vector2 } from 'three'
 
 export function useGlSelection() {
   const dispatch = useDispatch()
@@ -56,4 +60,89 @@ export function useGlSelection() {
     selectableScreenBounds,
     dispatch,
   ])
+}
+
+export function useDomSelection() {
+  const dispatch = useDispatch()
+
+  const isEnabled = useSelector(getIsSelectionEnabled)
+  const isSelecting = useSelector(getIsSelecting)
+  const startPoint = useSelector(getSelectionStartPoint)
+  const endPoint = useSelector(getSelectionEndPoint)
+
+  const handleStartSelection = React.useCallback(() => {
+    dispatch(doStartSelection())
+  }, [dispatch])
+
+  const handleEndSelection = React.useCallback(() => {
+    dispatch(doEndSelection())
+  }, [dispatch])
+
+  React.useEffect(() => {
+    if (!isEnabled) handleEndSelection()
+  }, [handleEndSelection, isEnabled])
+
+  React.useEffect(() => {
+    document.addEventListener('mousedown', handleMouseDown)
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+
+    return () => {
+      document.removeEventListener('mousedown', handleMouseDown)
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+
+    function handleMouseDown(ev: MouseEvent) {
+      console.log('selection down')
+      if (!isEnabled) return
+      if (ev.altKey) return
+      if (ev.button !== MOUSE.LEFT) return
+      handleStartSelection()
+      handleStart(ev)
+    }
+
+    function handleMouseMove(ev: MouseEvent) {
+      if (!isEnabled) return
+      if (!isSelecting) return
+      handleEnd(ev)
+    }
+
+    function handleMouseUp(ev: MouseEvent) {
+      console.log('selection up')
+      if (!isEnabled) return
+      if (!isSelecting) return
+      handleEndSelection()
+      handleEnd(ev)
+    }
+
+    function handleStart(ev: MouseEvent) {
+      dispatch(
+        doSetSelectionStartPoint({
+          x: (ev.clientX / window.innerWidth) * 2 - 1,
+          y: -(ev.clientY / window.innerHeight) * 2 + 1,
+        }),
+      )
+    }
+    function handleEnd(ev: MouseEvent) {
+      dispatch(
+        doSetSelectionEndPoint({
+          x: (ev.clientX / window.innerWidth) * 2 - 1,
+          y: -(ev.clientY / window.innerHeight) * 2 + 1,
+        }),
+      )
+    }
+  }, [
+    dispatch,
+    isEnabled,
+    isSelecting,
+    handleStartSelection,
+    handleEndSelection,
+  ])
+
+  return {
+    isSelecting,
+    startPoint,
+    endPoint,
+  }
 }
