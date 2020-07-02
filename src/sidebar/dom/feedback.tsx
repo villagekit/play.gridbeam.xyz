336b9d5a0aa308c5d57a3f061bc3532e448920e3
Michael Williams <michael.williams@enspiral.com>
2020-07-02 13:41:54 +1200

    simple feedback sidebar

--- src/sidebar/dom/feedback.tsx
+++ src/sidebar/dom/feedback.tsx
@@ -1,77 +1,46 @@
 /** @jsx jsx */
-import useMediaRecorder from '@wmik/use-media-recorder'
-import React from 'react'
-import { Box, jsx } from 'theme-ui'
+import Obfuscate from 'react-obfuscate'
+import { Flex, jsx, Text } from 'theme-ui'
 
 interface FeedbackProps {}
 
 export function DomSidebarFeedback(props: FeedbackProps) {
-  let {
-    error,
-    status,
-    mediaBlob,
-    stopRecording,
-    getMediaStream,
-    startRecording,
-  } = useMediaRecorder({
-    recordScreen: true,
-    blobOptions: { type: 'video/mp4' },
-    mediaStreamConstraints: {
-      audio: false,
-      video: {
-        displaySurface: 'browser',
-        cursor: 'always',
-      },
-    },
-  })
-
   return (
-    <article>
-      <h1>Screen recorder</h1>
-      {error ? `${status} ${error.message}` : status}
-      <section>
-        <button
-          type="button"
-          onClick={getMediaStream}
-          disabled={status === 'ready'}
-        >
-          Share screen
-        </button>
-        <button
-          type="button"
-          onClick={startRecording}
-          disabled={status === 'recording'}
-        >
-          Start recording
-        </button>
-        <button
-          type="button"
-          onClick={stopRecording}
-          disabled={status !== 'recording'}
-        >
-          Stop recording
-        </button>
-      </section>
-      <Player srcBlob={mediaBlob} />
-    </article>
+    <Flex sx={{ margin: 2, padding: 3, flexDirection: 'column' }}>
+      <FeedbackHeading>Give us feedback!</FeedbackHeading>
+      <FeedbackText>Love this app?</FeedbackText>
+      <FeedbackText>Found a problem?</FeedbackText>
+      <FeedbackText>Missing something?</FeedbackText>
+      <FeedbackText>Message us with feedback at:</FeedbackText>
+      <FeedbackText>
+        <Obfuscate
+          email="mikey@villagekit.com"
+          headers={{ subject: 'play.gridbeam.xyz feedback' }}
+        />
+      </FeedbackText>
+      <FeedbackText>Tell us what you love about this app.</FeedbackText>
+      <FeedbackText>
+        Tell us about the problem behavior, and what behavior you expected to
+        happen instead.
+      </FeedbackText>
+      <FeedbackText>Tell us what we should add to this app.</FeedbackText>
+      <FeedbackText>
+        If at all possible, please include screenshots or screen recordings, to
+        help us best understand your situation.
+      </FeedbackText>
+      <FeedbackText>Cheers!</FeedbackText>
+    </Flex>
   )
 }
 
-function Player({ srcBlob, audio }: any) {
-  if (!srcBlob) {
-    return null
-  }
+interface FeedbackHeadingProps extends React.ComponentProps<typeof Text> {}
 
-  if (audio) {
-    return <audio src={URL.createObjectURL(srcBlob)} controls />
-  }
+const FeedbackHeading = (props: FeedbackHeadingProps) => (
+  <Text sx={{ margin: 2, fontSize: 3, fontFamily: 'heading' }} {...props} />
+)
 
-  return (
-    <video
-      src={URL.createObjectURL(srcBlob)}
-      width={520}
-      height={480}
-      controls
-    />
-  )
-}
+interface FeedbackTextProps extends React.ComponentProps<typeof Text> {}
+
+const FeedbackText = (props: FeedbackTextProps) => (
+  <Text sx={{ margin: 2, fontSize: 2, fontFamily: 'body' }} {...props} />
+)
